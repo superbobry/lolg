a11877a95a54d560c2bcf6e7c8f0db6d6ddefaf2
Sergei Lebedev <superbobry@gmail.com>
2012-05-31 22:35:04 +0400

    Bumped 'cljsbuild' and 'google-closure-library' versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lolg "0.1.0-SNAPSHOT"
   :description "Nice and easy logging for ClojureScript."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/google-closure-library "0.0-790"]]
-  :plugins [[lein-cljsbuild "0.1.10"]]
+                 [org.clojure/google-closure-library "0.0-1376"]]
+  :plugins [[lein-cljsbuild "0.2.1"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:output-to "out/lolg.js"
                                    :output-dir "out"
