e3ed7c6ffbbbd870c1104303eba858e3b286c221
Sergei Lebedev <superbobry@gmail.com>
2014-03-10 20:31:20 +0400

    Updated 'project.clj' to the new 'lein-cljsbuild' format

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject lolg "0.1.0-SNAPSHOT"
   :description "Nice and easy logging for ClojureScript."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/google-closure-library "0.0-1376"]]
-  :plugins [[lein-cljsbuild "0.2.1"]]
-  :cljsbuild {:builds [{:source-path "src"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/google-closure-library "0.0-2029"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:builds [{:source-paths ["src"],
                         :compiler {:output-to "out/lolg.js"
                                    :output-dir "out"
                                    :optimizations :whitespace
